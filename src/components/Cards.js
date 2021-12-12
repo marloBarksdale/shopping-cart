937b1d1523331f02ef40c6bbe783b7d51b96a759
marloBarksdale <lyndenflood@gmail.com>
2021-12-12 18:15:34 -0500

    routing

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -13,7 +13,7 @@ const Cards = ({ products }) => {
     <>
       {products.map((product) => (
         <Link
-          to={`shopping-cart/shop/${params.category}/${product.id}/${product.title}`}
+          to={`/shop/${params.category}/${product.id}/${product.title}`}
           key={product.id}
         >
           <div className='card-container'>
