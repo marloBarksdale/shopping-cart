2262bbdc478d45933ea731cf7554cf885a7ba7d0
marloBarksdale <lyndenflood@gmail.com>
2021-12-12 04:56:14 -0500

    added counter for cart icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,19 +18,44 @@ const Navbar = ({ cart, sidebar, toggleSideBar, count, total, addToCart }) => {
         </nav>
         <Link to='/'>
           {' '}
-          <h1>Hello from App</h1>
+          <h1>
+            ILove <FaIcons.FaHeartBroken style={{ color: 'red' }} /> Fake
+          </h1>
         </Link>
         <nav>
-          <Link to='#'>
+          <Link to='#' style={{ position: 'relative', fontSize: '1.5rem' }}>
             <FaIcons.FaShoppingBag
               onClick={() => {
                 handleClick(true);
               }}
             />
+
+            {count > 0 ? (
+              <span
+                onClick={() => {
+                  handleClick(true);
+                }}
+                style={{
+                  position: 'absolute',
+                  backgroundColor: '#00bcd4',
+                  borderRadius: '9999px',
+                  width: '1.5rem',
+                  height: '1.5rem',
+                  textAlign: 'center',
+                  fontSize: '1rem',
+                  right: '71%',
+                  bottom: '-9%',
+                }}
+              >
+                {count}
+              </span>
+            ) : (
+              ''
+            )}
           </Link>
           |{' '}
           <Link to='/profile'>
-            <FaIcons.FaUserAlt />
+            <FaIcons.FaUserAlt style={{ fontSize: '1.5rem' }} />
           </Link>
           <nav className={sidebar ? 'cart-container active' : 'cart-container'}>
             <div
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import getData from '../data';
 import { useParams } from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
 
 const Product = ({ toggleSideBar, addToCart }) => {
   const [productsData, setProductData] = useState([]);
+  const [quantity, setQuantity] = useState(1);
 
   let params = useParams();
 
@@ -20,7 +22,7 @@ const Product = ({ toggleSideBar, addToCart }) => {
 
   const handleClick = (filter, state) => {
     toggleSideBar(state);
-    addToCart(filter, 1);
+    addToCart(filter, quantity);
   };
 
   return (
@@ -38,6 +40,41 @@ const Product = ({ toggleSideBar, addToCart }) => {
             </div>
             <div>${filter.price}</div>
             <p>{filter.description}</p>
+
+            <div>
+              <h3>Quantity</h3>
+
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <FaIcons.FaRegMinusSquare
+                  style={{
+                    fontSize: '1.5rem',
+                  }}
+                  onClick={() => {
+                    if (quantity === 1) {
+                      return;
+                    }
+                    setQuantity((prev) => prev - 1);
+                  }}
+                />
+                <span
+                  style={{
+                    width: '2.5rem',
+                    textAlign: 'center',
+                    padding: '0.75rem',
+                  }}
+                >
+                  {quantity}
+                </span>
+                <FaIcons.FaRegPlusSquare
+                  style={{
+                    fontSize: '1.5rem',
+                  }}
+                  onClick={() => {
+                    setQuantity((prev) => prev + 1);
+                  }}
+                />
+              </div>
+            </div>
             <button
               onClick={() => {
                 handleClick(filter, true);
